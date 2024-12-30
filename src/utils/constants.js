b4cf42dd282eefd5c5ef358e5b2cbd2eba9611dc
Furquan Anwer <furquan.anwer@gmail.coom>
2024-12-30 23:38:36 +0530

    change live chat constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const LIVE_CHAT_COUNT = 13;
 export const YOUTUBE_VIDEO_API = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails&part=id&chart=mostPopular&maxResults=50&key=AIzaSyBm1dmqgLQyiooa6khtY6kVNZlye8nw2LM&part=snippet&part=statistics";
 export const YOUTUBE_SEARCH_RESULTS_API = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&key=AIzaSyBm1dmqgLQyiooa6khtY6kVNZlye8nw2LM&q=";
 export const YOUTUBE_SUGGESTIONS = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+
